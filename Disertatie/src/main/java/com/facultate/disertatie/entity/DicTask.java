529079413342a0419e4a17d640d6d9ba133d6b81
Petcu Barbu <barbu.petcu@gmail.com>
2018-09-02 05:41:41 +0300

    task backend

---
+++ Disertatie/src/main/java/com/facultate/disertatie/entity/DicTask.java
@@ -0,0 +1,157 @@
+package com.facultate.disertatie.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@Table(name="dic_task")
+public class DicTask{
+	@Id
+	@Column(name="id", nullable=false, updatable=false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="id_task_seq")
+	@SequenceGenerator(name="id_task_seq", sequenceName = "id_task_seq", allocationSize=1)
+	private long id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="deadline")
+	private LocalDateTime deadline;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="end_date")
+	private LocalDateTime end_date;
+	
+	@JsonIgnore
+    @Column(name="created", updatable=false)
+	@CreationTimestamp
+	private LocalDateTime created;
+    
+    @JsonIgnore
+	@Column(name="modified")
+	@UpdateTimestamp
+	private LocalDateTime modified;
+    
+    @ManyToOne
+    @JoinColumn(name="id")
+    private RefDifficulty difficulty;
+    
+    @ManyToOne
+    @JoinColumn(name="id")
+    private RefPriority priority;
+    
+    @ManyToOne
+    @JoinColumn(name="id")
+    private RefTaskStatus status;
+	
+    @ManyToOne
+    @JoinColumn(name="id")
+    private RefTaskEnd end_status;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(LocalDateTime deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(LocalDateTime end_date) {
+		this.end_date = end_date;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getModified() {
+		return modified;
+	}
+
+	public void setModified(LocalDateTime modified) {
+		this.modified = modified;
+	}
+
+	public RefDifficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(RefDifficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public RefPriority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(RefPriority priority) {
+		this.priority = priority;
+	}
+
+	public RefTaskStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RefTaskStatus status) {
+		this.status = status;
+	}
+
+	public RefTaskEnd getEnd_status() {
+		return end_status;
+	}
+
+	public void setEnd_status(RefTaskEnd end_status) {
+		this.end_status = end_status;
+	}
+        
+}
